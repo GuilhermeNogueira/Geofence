0e66a72ab8de391b112e8c46f273009f0b731e15
Guilherme Nogueira <guilherme.nogueira@ifood.com.br>
2021-05-26 12:18:32 -0300

    Adding 7 digits validation for geofence creation

--- src/main/kotlin/com/nogueira/geofence/adapters/api/model/GeofenceCreateRequest.kt
+++ src/main/kotlin/com/nogueira/geofence/adapters/api/model/GeofenceCreateRequest.kt
@@ -3,20 +3,24 @@ package com.nogueira.geofence.adapters.api.model
 import com.nogueira.geofence.core.domain.Advertising
 import com.nogueira.geofence.core.domain.Geofence
 import com.nogueira.geofence.core.domain.Point
+import com.nogueira.geofence.core.roundTo
 
 data class GeofenceCreateRequest(
     val radius: Int,
     val name: String,
     val location: PointRequest
 ) {
-
     companion object {
         fun GeofenceCreateRequest.toGeofence() =
             Geofence(null, this.radius, Point(this.location.lat, this.location.lng), this.name)
     }
 }
 
-data class PointRequest(val lat: Double, val lng: Double)
+class PointRequest(lat: Double, lng: Double) {
+    //rounding to 7 digits max
+    val lat: Double = lat.roundTo(7)
+    val lng: Double = lng.roundTo(7)
+}
 
 
 data class AdvertisingCreateRequest(
